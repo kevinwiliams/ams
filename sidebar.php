6d9b1096349a277ef0976ddaea99e6c2eaa2d5bc
Kevin Williams <kwilliams9285@gmail.com>
2025-03-21 15:58:42 -0500

    Test

--- sidebar.php
+++ sidebar.php
@@ -10,7 +10,7 @@ if (session_status() === PHP_SESSION_NONE) {
 $user_role = $_SESSION['role_name'];
 $create_roles = ['Manager', 'ITAdmin', 'Editor', 'Dept Admin', 'Security','Op Manager', 'Broadcast Coordinator' ];
 $report_roles = ['Manager', 'ITAdmin', 'Editor', 'Dept Admin','Op Manager' ];
-$show_roles = ['Op Manager','ITAdmin' ];
+$show_roles = ['Op Manager','ITAdmin'];
 $it_roles = ['ITAdmin'];
 
 
