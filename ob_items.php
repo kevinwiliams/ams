98c0791368dccf72945b3a6616369c4b1b90e80e
Kevin Williams <kwilliams9285@gmail.com>
2025-03-30 09:29:40 -0500

    hide delete button

--- ob_items.php
+++ ob_items.php
@@ -99,7 +99,7 @@ $items = $obItem->getAllItems();
                                         data-desc="<?= htmlspecialchars($item['description']) ?>">
                                         <i class="fas fa-edit"></i>
                                 </button>
-                                <button class="btn btn-sm btn-danger deleteItem" data-id="<?= $item['item_id'] ?>"><i class="fas fa-trash"></i></button>
+                                <!-- <button class="btn btn-sm btn-danger deleteItem" data-id="<?= $item['item_id'] ?>"><i class="fas fa-trash"></i></button> -->
                             </td>
                         </tr>
                     <?php endforeach; ?>
